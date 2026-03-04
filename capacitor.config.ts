d8f592b66a2fe56ef208f7f9e9156c0bfd8f8ebc
MsLau972 <81325002+MsLau972@users.noreply.github.com>
2026-03-04 14:34:20 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kahoute',
+  webDir: 'www'
+};
+
+export default config;
